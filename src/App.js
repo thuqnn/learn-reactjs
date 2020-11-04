afd0da7920c9143a3ca27b3ff7a9061aa4ae3bb2
thuqnn <phamthithuqn92@gmail.com>
2020-11-05 01:17:58 +0700

    CreateTitle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          THU PHAM 92
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Thu Pham React JS Basic</h1>
       </header>
     </div>
   );
