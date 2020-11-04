105b4ec2e67ad2c167f9fef7fc840e1ce0586ea3
thuqnn <phamthithuqn92@gmail.com>
2020-11-05 01:06:50 +0700

    onechange

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THU PHAM 92
         </p>
         <a
           className="App-link"
